d07919cc26d9b796fa2fc419b93df0ccdd41ef8d
unknown <izuagbalachere@gmail.com>
2024-05-01 05:31:31 +0100

    Database and Docker set up

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Sales Management System: Phase 1");
 });
 
 app.listen(port, () => {
